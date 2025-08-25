d5fabdf5291f6acafbbf203b5ec08f0cfd23e311
Kingsley-codes <agbamkingsley@gmail.com>
2025-08-25 19:38:35 +0100

    mod

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required for static exports with images
+  },
 };
 
 export default nextConfig;
